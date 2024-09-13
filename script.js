859ff343303738cae7544115413ed4f69ca76e3f
Kenan_E <130177718+Kenan-E@users.noreply.github.com>
2024-09-14 00:18:56 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    document.querySelector('.container').style.opacity = '1';
+    document.querySelector('.container').style.transform = 'translateY(0)';
+
+    // Fade-in and slide-up the container
+    container.style.opacity = '1';
+    container.style.transform = 'translateY(0) scale(1)';
+});
